b7571b4be9dc9d4cfaebd71e3cbb6362452887c1
alexiusvdt <alex.johnson293@gmail.com>
2023-06-14 16:53:38 -0700

    add play button to initialize game

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 
 class AlienInvasion:
@@ -35,6 +36,8 @@ class AlienInvasion:
 
         self._create_fleet()
 
+        self.play_button = Button(self, "Play")
+
 
     def run_game(self):
         """start game loop"""
@@ -57,7 +60,14 @@ class AlienInvasion:
                 self._check_keydown_events(event)            
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)  
-        
+            elif event.type ==pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+
     def _check_keydown_events(self, event):
         # respond to key presses
         if event.key == pygame.K_RIGHT:
@@ -106,6 +116,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # make most recently drawn screen visible
         pygame.display.flip()
