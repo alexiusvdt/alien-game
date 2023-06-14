2c324bd8812b6c2c6919c810cbddbd239f84b3c2
alexiusvdt <alex.johnson293@gmail.com>
2023-06-14 15:51:52 -0700

    add entire fleet generation logic

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """initialize"""
         #screen
         self.screen_width = 1200
-        self.screen_height = 600
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
         # ship settings
