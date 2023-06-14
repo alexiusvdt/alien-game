83714726ce52620658acdfb42e848eb7f01544b9
alexiusvdt <alex.johnson293@gmail.com>
2023-06-14 12:33:30 -0700

    refactor settings into separate module

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,23 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Game-level class to manage assets & behavior"""
 
     def __init__(self):
         """Initialize game & create resources"""
         pygame.init()
+        self.settings = Settings()
 
         # display size set up, this can be changed later but hardcoded for now
         # in pygame, every element is its own surface
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
 
-        # background color
-        self.bg_color = ( 230, 230, 230 )
 
     def run_game(self):
         """start game loop"""
@@ -25,7 +28,7 @@ class AlienInvasion:
                     sys.exit()
                 
                 # redraw the screen during each pass w/ the color prop
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
 
                 # make most recently drawn screen visible
                 pygame.display.flip()
